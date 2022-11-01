e811816f9b04cdf196664a3c9436f2dae96c8166
Chinmaya Narayana <58813915+chinmayaNK22@users.noreply.github.com>
2022-11-01 18:55:45 +0530

    Update VarClassFDR.py

--- VarClassFDR.py
+++ VarClassFDR.py
@@ -5,11 +5,11 @@ from class_FDR import class_fdr
 import os
 from pdResult_parser import fetch_sqlite3_db
 
-infile = "..\PRIDE\M_avium_Mavium_hominissuis_variant_proteome_search_082021.pdResult"
+infile = "F:\\NTNU_M_avium_Proteomics\\PRIDE\\M_avium_Mavium_hominissuis_variant_proteome_search_082021.pdResult"
 
-var_fasta = "Mavium_hominissuis_OCU464_GCF_001865635.4_ASM186563v4_protein_variant_DB_082021.fasta"
+var_fasta = "F:\\NTNU_M_avium_Proteomics\\classFDR\\Mavium_hominissuis_OCU464_GCF_001865635.4_ASM186563v4_protein_variant_DB_082021.fasta"
 
-fastas = "."
+fastas = "F:\\NTNU_M_avium_Proteomics\\classFDR"
 
 def get_header_index(inlist):
     pro = inlist.index('ParentProteinAccessions')
@@ -22,9 +22,14 @@ def get_header_index(inlist):
 
     xcorr = inlist.index('XCorr')
 
+    try:
+        msamandascore = inlist.index('AmandaScore')
+    except:
+        raise ("The search result does not contain AmandaScore")
+
     fdr = inlist.index('PercolatorqValue')
 
-    return pro, pep, mod_pep, mod, xcorr, fdr
+    return pro, pep, mod_pep, mod, xcorr, fdr, msamandascore
 
 def digest_fasta(infasta):
     dicts = {}
@@ -60,11 +65,11 @@ def read_fasta(inpath, var_fasta):
         if os.path.isfile(os.path.join(inpath, files)):
             if files.split('.')[-1] in extension:
                 if files != os.path.split(var_fasta)[-1]:
-                    digested_peps |= digest_fasta(files)
+                    digested_peps |= digest_fasta(os.path.join(inpath, files))
                 
     return digested_peps
    
-def get_file_info(infile, pep_type):
+def get_file_info(infile, pep_type, search_engine):
     peps = read_fasta(fastas, var_fasta)
     mut_peps = digest_fasta(var_fasta)
     if pep_type == 'target':
@@ -75,16 +80,20 @@ def get_file_info(infile, pep_type):
             mod_pep = i[a[2]]
             pro = i[a[0]]
             mod = i[a[3]]
-            xcorr = i[a[4]]
+            score = ""
+            if search_engine == 'sequest':
+                score = i[a[4]]
+            elif search_engine == 'amanda':
+                score = i[a[-1]]
             q_val = i[a[5]]
-            if xcorr != None:
+            if score != None:
                 pro = ""
-                if pep in peps:
-                    pro = ';'.join(peps[pep])
-                elif pep in mut_peps:
-                    pro = ';'.join('SAAV@' + p for p in mut_peps[pep])
+                if pep.upper() in peps:
+                    pro = ';'.join(peps[pep.upper()])
+                elif pep.upper() in mut_peps:
+                    pro = ';'.join('SAAV@' + p for p in mut_peps[pep.upper()])
 
-                yield mod_pep + '_' + mod, pro, xcorr, q_val
+                yield mod_pep + '_' + mod, pro, score, q_val
 
     elif pep_type == 'decoy':
         header, table = fetch_sqlite3_db(infile, pep_type)
@@ -94,33 +103,40 @@ def get_file_info(infile, pep_type):
             mod_pep = i[a[2]]
             pro = i[a[0]]
             mod = i[a[3]]
-            xcorr = i[a[4]]
+            score = ""
+            if search_engine == 'sequest':
+                score = i[a[4]]
+            elif search_engine == 'amanda':
+                score = i[a[-1]]
             q_val = i[a[5]]
-            if xcorr != None:
-                if 'decoy_' + pep in peps:
-                    pro = ';'.join(peps['decoy_' + pep])
-                elif 'decoy_' + pep in mut_peps:
-                    pro = ';'.join('XXX_SAAV@'+ p.lstrip('XXX_') for p in mut_peps['decoy_' + pep])
+            if score != None:
+                if 'decoy_' + pep.upper() in peps:
+                    pro = ';'.join(peps['decoy_' + pep.upper()])
+                elif 'decoy_' + pep.upper() in mut_peps:
+                    pro = ';'.join('XXX_SAAV@'+ p.lstrip('XXX_') for p in mut_peps['decoy_' + pep.upper()])
                     
-                yield mod_pep + '_' + mod, pro, xcorr, q_val
+                yield mod_pep + '_' + mod, pro, score, q_val
 
-output = []
-for info in get_file_info(infile, 'target'):
-    output.append(info)
+def run_varclassfdr(infile, search_engine):
+    output = []
+    for info in get_file_info(infile, 'target', search_engine):
+        output.append(info)
     
-for info in get_file_info(infile, 'decoy'):
-    output.append(info)
+    for info in get_file_info(infile, 'decoy', search_engine):
+        output.append(info)
 
-scores = {idx:float(o[2]) for idx, o in enumerate(output)}
+    scores = {idx:float(o[2]) for idx, o in enumerate(output)}
 
-sort_output = [output[k] for k, v in sorted(scores.items(), key=lambda item: item[1], reverse=True)]
+    sort_output = [output[k] for k, v in sorted(scores.items(), key=lambda item: item[1], reverse=True)]
 
-result = class_fdr(sort_output)
-
-outfile = "{0}_classFDR_percolatorFDR.txt".format(infile.rstrip(infile.split('.')[-1]).rstrip('.'))
-with open(outfile, 'w') as outf:
-   outf.write('Peptide\tModification\tProtein\tXcorr\tPercolator_qvalue\tclass_FDR\tFDP\tclassFDR_error\n')
-   outf.writelines('\t'.join(i) + '\n' for i in result)
-
-
+    inpath = os.path.split(infile)[0]
     
+    result = class_fdr(inpath, sort_output, search_engine)
+
+    outfile = "{0}_SequestHT_classFDR.txt".format(infile.rstrip(infile.split('.')[-1]).rstrip('.'))
+    with open(outfile, 'w') as outf:
+       outf.write('Peptide\tModification\tProtein\tXcorr\tPercolator qvalue\tclass-FDR\tFalse Discovery Proportion (FDP)\tclass-FDR error\n')
+       outf.writelines('\t'.join(i) + '\n' for i in result)
+
+
+run_varclassfdr(infile, 'amanda')    
